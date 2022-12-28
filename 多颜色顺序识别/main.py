1875c369bb71c73d75e4bef15bdb3b80080d576c
玄学 <92659074+KW-T@users.noreply.github.com>
2022-12-28 14:45:25 +0800

    Update main.py

--- 多颜色顺序识别/main.py
+++ 多颜色顺序识别/main.py
@@ -7,22 +7,20 @@ import sensor, image, time
 #引入三个库
 red = (55, 0, 31, 113, -22, 84)
 green = (0, 100, -26, -128, 127, 3)
-blue = (38, 7, 4, 118, -101, -20)
 ## 定义三个颜色的lab值范围
-colour = [red,green,blue]
+colour = [red,green]
 #一个颜色组colour
 red_blob = None
 green_blob = None
-blue_blob = None
-##定义三个空的变量，用来盛放下面寻找到的色块的信息
-blobs_group = [red_blob,green_blob,blue_blob]
+##定义2个空的变量，用来盛放下面寻找到的色块的信息
+blobs_group = [red_blob,green_blob]
 # 定义一个色块组blobs_group
-Name_list = ['R','G','B']
-Order_list= [1,2,3]
-#定义顺序列表，打印时信息为1,2,3，
+Name_list = ['R','G']
+Order_list= [1,2]
+#定义顺序列表，打印时信息为1,2,
 tool = []
-i,j,t= 0,0,2
-### 定义三个变量并赋值，最后比较色块x坐标大小时会用到
+i,j= 0,1
+### 定义2个变量并赋值，最后比较色块x坐标大小时会用到
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 #定义图像为彩色
@@ -38,7 +36,7 @@ uart = UART(3, 115200)
 def Find_group():
     t = 0
     #定义一个发现色块组的函数
-    while t < 3:
+    while t < 2:
     #find_blobs函数会寻找对应阈值内颜色的色块
         img.draw_line(5,30,150,30)
         img.draw_line(5,100,150,100)
@@ -54,24 +52,24 @@ def Find_group():
                     img.draw_rectangle(b[0:4])
                     img.draw_cross(b[5], b[6])
                     img.draw_string(b[0], b[1], Name_list[t])
-                    #依次寻找红绿蓝色块
+                    #依次寻找红绿色块
                     tool.append(b[5])
         t+=1
 while (True):
     clock.tick()
     img = sensor.snapshot()
     #抓取画面，然后用前面定义的好的Find_group函数抓取出画面中的色块
-    Order_list= [1,2,3]
-    #判断是否抓取到三个色块
+    Order_list= [1,2]
+    #判断是否抓取到2个色块
     tool = []
     Find_group()
-    if len(tool) == 3 :
+    if len(tool) == 2 :
     #在openmv中坐标原点是左上角，x轴向右，y轴向下。
-        for i in range(0,2):
+        for i in range(0,1):
         #所以对tool中数据进行排序的同时，也根据tool中数据顺序的变化更改Order_list对应的数据顺序
             j=i+1
             #  这样我们就得到了排列好的Order_list的顺序，即三个色块的排列顺序
-            for j in range(1,3):
+            for j in range(1,2):
             #为了避免循环程序时，会对已经排序好的Order_list再次进行排序，
                 if tool[i]>tool[j] :
                     tool[i],tool[j] = tool[j],tool[i]
